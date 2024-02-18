ade00748e40576ab166c857d3ab1b2e98ed37337
LeeJugnmin <hiihjm104@gmail.com>
2024-02-18 20:34:07 +0900

    Durain

---
+++ src/test/java/com/durianfirst/durian/DurianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durianfirst.durian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DurianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
